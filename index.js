3a720786ddf2bf49837b4cfdf92d22bbc19c03ae
Diana Cardona <dscardona@gmail.com>
2023-06-08 16:00:14 -0400

    Add and test btn event listener

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputFieldEl = document.getElementById("input-field")
+const addBtn = document.getElementById("add-button")
+
+addBtn.addEventListener("click", function() {
+    console.log("clicked!")
+})
