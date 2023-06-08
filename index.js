66cc5d66ef3ea5cc818411464f450b2479a33ad4
Diana Cardona <dscardona@gmail.com>
2023-06-08 16:59:25 -0400

    Connect app to Firebase project database

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const inputFieldEl = document.getElementById("input-field")
-const addBtn = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-addBtn.addEventListener("click", function() {
+const appSettings = {
+    databaseURL: "https://realtime-database-62087-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
     console.log(inputValue)
 })
